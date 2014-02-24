1429c6e965c394f44b316bd98603c193218952b7
xinlei <wang.xinlei15@gmail.com>
2014-02-24 01:06:35 -0800

    Edited Question abstract class

---
+++ src/model/MultipleChoice.java
@@ -0,0 +1,17 @@
+package model;
+
+public class MultipleChoice extends Question {
+
+	@Override
+	String[] getAnswer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	int getPoints(String[] answers) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
