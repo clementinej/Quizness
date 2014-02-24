16de69a008f01d2925feb5c65f9aa58104a55c99
gene <gene.oetomo@gmail.com>
2014-02-24 03:21:49 -0800

    nothing changed

--- src/model/FillInTheBlank.java
+++ src/model/FillInTheBlank.java
@@ -1,16 +1,32 @@
 package model;
 
+import java.util.Set;
+
 public class FillInTheBlank extends Question {
 
+	private String question;
+	private Set<String> answers;
+	private int numBlanks;
+	
+	public FillInTheBlank(String question, Set<String> answers, int numBlanks){
+		this.question = question;
+		this.answers = answers;
+		this.numBlanks = numBlanks;
+	}
+	
 	@Override
 	String[] getAnswer() {
-		// TODO Auto-generated method stub
-		return null;
+		String[] answers = new String[this.answers.size()];
+		int index = 0;
+		for (String answer : answers){
+			answers[index] = answer;
+		}
+		return answers;
 	}
 
 	@Override
 	int getPoints(String[] answers) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
