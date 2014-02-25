8bfac8f0a0e9ee8851457e1ad0b16ad46d750453
lglucin <lglucin@gmail.com>
2014-02-25 02:32:20 -0800

    Moved WebContent folder to Java Resources

--- src/model/Answers.java
+++ src/model/Answers.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.sun.tools.javac.util.List;
+//import com.sun.tools.javac.util.List;
 
 public class Answers {
 	
