3c3dd19a99d7942224fa80ec5b9d03e6a9228e25
xinlei <wang.xinlei15@gmail.com>
2014-03-01 11:08:01 -0800

    Inbox
    Message m = Inbox.getMessage(messageID);
    int numRequests = Inbox.numFriendReqs(userID);
    int numMessages = Inbox.numNewMessages(userID);
    int numChallenges = Inbox.numChallenges(userID);

---
+++ src/model/Inbox.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Inbox {
+
+	
+	public Message getMessage(int messageID){
+		return null; 
+	}
+	
+	public int numFriendReqs(int userID){
+		return 0;
+	}
+	
+	public int numNewMessages(int userID){
+		return 0;
+	}
+	
+	public int  numChallenges(int userID){
+		return 0; 
+	}
+}
