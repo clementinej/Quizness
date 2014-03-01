d489792546cff5337839bb95f6fddd8506a00b58
lglucin <lglucin@gmail.com>
2014-03-01 14:34:04 -0800

    checking to see if commit will update control

--- src/control/CreateAccountServlet.java
+++ src/control/CreateAccountServlet.java
@@ -42,9 +42,10 @@ public class CreateAccountServlet extends HttpServlet {
 
 		PrintWriter out = response.getWriter();
 		
-		if(manager.createNewAccount(userName, pw, false)) {//go to "user welcome" page
-			RequestDispatcher dispatch = request.getRequestDispatcher("home.html"); 
-			dispatch.forward(request, response); 
+		try {
+			if(manager.createNewAccount(userName, pw, false)) {//go to "user welcome" page
+				RequestDispatcher dispatch = request.getRequestDispatcher("home.html"); 
+				dispatch.forward(request, response); 
 //			out.println("<!DOCTYPE html>");
 //			out.println("<html>");
 //			out.println("<head>");
@@ -54,22 +55,26 @@ public class CreateAccountServlet extends HttpServlet {
 //			out.println("<h1>Welcome " + userName + "</h1>");
 //			out.println("</body>");
 //			out.println("</html>");
-			
-		} else {//go to "Account Name is in Use" page		
-			out.println("<!DOCTYPE html>");
-			out.println("<html>");
-			out.println("<head>");
-			out.println("<title>Create Account</title>");
-			out.println("</head>");
-			out.println("<body>");
-			out.println("<h1>The Name " + userName + " is Already In Use</h1>");
-			out.println("<p>Please enter another user name and password.</p>");
-			out.println("<form action=\"CreateAccountServlet\" method=\"post\">");
-			out.println("<p>User Name: <input type=\"text\" name=\"user name\" /></p>");
-			out.println("<p>Password: <input type=\"text\" name=\"password\"/><input type=\"submit\" value=\"Login\"/></p>");
-			out.println("</form>");
-			out.println("</body>");
-			out.println("</html>");	
+				
+			} else {//go to "Account Name is in Use" page		
+				out.println("<!DOCTYPE html>");
+				out.println("<html>");
+				out.println("<head>");
+				out.println("<title>Create Account</title>");
+				out.println("</head>");
+				out.println("<body>");
+				out.println("<h1>The Name " + userName + " is Already In Use</h1>");
+				out.println("<p>Please enter another user name and password.</p>");
+				out.println("<form action=\"CreateAccountServlet\" method=\"post\">");
+				out.println("<p>User Name: <input type=\"text\" name=\"user name\" /></p>");
+				out.println("<p>Password: <input type=\"text\" name=\"password\"/><input type=\"submit\" value=\"Login\"/></p>");
+				out.println("</form>");
+				out.println("</body>");
+				out.println("</html>");	
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
