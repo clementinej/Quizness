1ed2c6ee8bd9274ab3c5e4cfd8c7f23f2dd38b1f
xinlei <wang.xinlei15@gmail.com>
2014-03-07 21:20:39 -0800

    Add average score methods in  QuizResult

--- src/model/QuizResult.java
+++ src/model/QuizResult.java
@@ -3,6 +3,7 @@ package model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class QuizResult {
@@ -24,6 +25,38 @@ public class QuizResult {
 		return executeQuery(query);
 	}
 	
+	// Return the average score of this user
+	public static double getUserAverageScore(int userID, int quizID) throws Exception{
+		String query = "SELECT AVG(score)  FROM quizTries WHERE userID = " + userID
+				+ " AND WHERE quizID = " + quizID;
+		PreparedStatement ps = ServerConnection.getConnection().prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) return rs.getDouble(1);
+		else return -1.0; 
+	}
+	
+	// Return the average score of all users
+	public static double getAllAverageScore(int quizID) throws Exception {
+		String query = "SELECT AVG(score)  FROM quizTries WHERE quizID = " + quizID;
+		PreparedStatement ps = ServerConnection.getConnection().prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) return rs.getDouble(1);
+		else return -1.0; 
+	}
+	
+	// Return the average score of this user's friends
+	public static double getFriendsAverageScore(int userID, int quizID) throws Exception {
+		String query = "SELECT AVG(score) FROM quizTries WHERE quizID = " + quizID
+				+ " INNER JOIN friendships USING (toID) WHERE fromID = " + userID;
+		PreparedStatement ps = ServerConnection.getConnection().prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) return rs.getDouble(1);
+		else return -1.0; 
+	}
+	
+	// TODO: timespent for user, everyone, user's friend average time
+	// TODO: number of attempts for user, everyone, user's friend average attempts
+	
 	// Execute the given query, throws exception=
 	private static ArrayList<Integer> executeQuery(String query) throws Exception{
 		Connection con = ServerConnection.getConnection();
@@ -40,6 +73,5 @@ public class QuizResult {
 		}
 		return result; 
 	}
-	
 	// average performance specifications
 }
